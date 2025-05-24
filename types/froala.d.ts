f570d179dfde2e13c6b21d661ac5e28e9b9c6502
daiarobert <daiarobert2@gmail.com>
2025-05-24 19:18:09 +0100

    added types for froala

---
+++ types/froala.d.ts
@@ -0,0 +1,4 @@
+declare module "froala-editor/js/plugins.pkgd.min.js";
+declare module "froala-editor/css/froala_editor.pkgd.min.css";
+declare module "froala-editor/css/froala_style.min.css";
+declare module "froala-editor/css/themes/gray.min.css";
